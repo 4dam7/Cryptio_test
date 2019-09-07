86362b4bcd7211bae8fb0e87dd305175f5bc571c
Adam Cohen <acohen@linux.home>
2019-09-08 01:09:43 +0200

    add: react build

---
+++ src/build/precache-manifest.a8a1f254762ae46aa1131331735530f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a62439c25d77113f7a5d848bda4d04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eef28c920374e3a90709",
+    "url": "/static/css/main.87efd7d7.chunk.css"
+  },
+  {
+    "revision": "03992fc2f115f58f8478",
+    "url": "/static/js/2.5e33dfaa.chunk.js"
+  },
+  {
+    "revision": "eef28c920374e3a90709",
+    "url": "/static/js/main.1a0b85bc.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "47be1122afe334a42a2908f7c1c42477",
+    "url": "/static/media/cryptio.47be1122.png"
+  }
+]);
